8144315c9602fbcd63f70301f407ec2d37b6e240
Dagoshi85R <132560587+Dagoshi85R@users.noreply.github.com>
2026-09-03 12:04:57 +0100

    Update Z_histograms1.515.py

--- Z_histograms1.0.py
+++ Z_histograms1.0.py
@@ -1242,7 +1242,7 @@ else:
             st.warning("No 'Colocalization' measurement files detected.")
         else:
             st.info("""💡 **Note:** Pie charts display the percentage of unique particles colocalized versus particles detected strictly in a single channel.
-            Your percentages may differ slightly from ZetaSphere's pdf report. This is expected! This app process displays the percentages of all your raw data, while ZetaSphere applies a diffussion correction miltiplier to compensate for small particles drifting out of the camera's field of view. This app prioritizes 100% mathematical transparency by plotting exactly what is in your data file.""")
+            Your percentages may differ slightly from ZetaSphere's pdf report. This is expected! This app process displays the percentages of all your raw data, while ZetaSphere applies a diffussion correction multiplier to compensate for small particles drifting out of the camera's field of view. This app prioritizes 100% mathematical transparency by plotting exactly what is in your data file.""")
             
             active_coloc_items = []
             for ch, items in processed_data['Colocalization'].items():
