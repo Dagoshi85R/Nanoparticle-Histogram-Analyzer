d283be24fb6dca4bd1091f4b6322e42c9bc97a4b
Dagoshi85R <132560587+Dagoshi85R@users.noreply.github.com>
2026-09-10 15:16:08 +0100

    Update Z_histograms1.525.py

--- Z_histograms1.0.py
+++ Z_histograms1.0.py
@@ -126,13 +126,13 @@ with st.sidebar:
     link_radius = 10.0
 
     # --- NEW: Help Guide Link Button ---
-        st.sidebar.markdown("---")
-        st.sidebar.subheader("📖 Documentation")
-        st.sidebar.link_button(
-            label="View Help Guide (PDF)", 
-            url="https://github.com/Dagoshi85R/Nanoparticle-Histogram-Analyzer/blob/main/Histogram_Analyzer_1.514.pdf",
-            use_container_width=True
-        )
+    st.markdown("---")
+    st.subheader("📖 Documentation")
+    st.link_button(
+        label="View Help Guide (PDF)", 
+        url="https://github.com/Dagoshi85R/Nanoparticle-Histogram-Analyzer/blob/main/Histogram_Analyzer_1.514.pdf",
+        use_container_width=True
+    )
 
 # --- Helper Functions ---
 def parse_file_info(uploaded_file):
